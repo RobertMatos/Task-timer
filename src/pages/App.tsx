8d6b4d963ab0de269ed0af0e7d8d744754d5cffe
Robert <robertvmatos@gmail.com>
2022-04-24 21:48:06 -0300

    Upando o projeto

---
+++ src/pages/App.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import Cronometro from "../components/Cronometro";
+import Formulario from "../components/Formulario";
+import Lista from "../components/Lista";
+import { ITarefa } from "../types/tarefa";
+import style from "./App.module.scss";
+import Helmet from 'react-helmet';
+
+function App() {
+  
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas((tarefasAnteriores) =>
+      tarefasAnteriores.map((tarefa) => ({
+        ...tarefa,
+        selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+      }))
+    );
+  }
+
+  function finalizarTarefa() {
+    if(selecionado) {
+      setSelecionado(undefined);
+      setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => {
+        if(tarefa.id === selecionado.id) {
+          return {
+            ...tarefa,
+            selecionado: false,
+            completado: true
+          }
+        }
+        return tarefa;
+      }))
+    }
+  }
+
+  return (
+    <div className={style.AppStyle}>
+      <Helmet title="Temporizador de tarefas" />
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+      <Cronometro 
+        selecionado={selecionado
+        } 
+        finalizarTarefa={finalizarTarefa}
+      />
+    </div>
+  );
+}
+
+export default App;
